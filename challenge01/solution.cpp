5028fca87b34d623fe83e9afd01d1367c113c969
Elias Tadesse Tessema <ckl422@tesla15.eecs.utk.edu>
2025-01-24 16:15:48 -0500

    added comments

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -1,12 +1,23 @@
-// Challenge 01: Rotating Arrays; null solution
+/* Challenge 01: Rotating Arrays; null solution
+ *
+ * Elias Tessema NETID: CKL422
+ *
+ * This program will rotate a given list of numbers by the given direction and 
+ * number of rotations
+*/
 
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    int numVals = 0, numRotations = 0;
+    
+	// Input variables
+	int numVals = 0, numRotations = 0;
     char dir = 'X';
+
+	// Iterator and new index variables
     int i = 0, index = 0;
 
+	// For every set of inputs in the cin stream
     while(std::cin >> numVals >> numRotations >> dir){
         int nums[numVals];
         int newNums[numVals];
@@ -17,8 +28,10 @@ int main(int argc, char *argv[]) {
 
         if (dir == 'L'){
             for (i = 0; i < numVals; i++){
-                index = i - numRotations;
-                while (index < 0){
+				index = i - numRotations;
+				
+				// Until index is greater than 0, add the numbers of values
+				while (index < 0){
                     index = numVals + index;
                 }
                 newNums[index] = nums[i];
@@ -28,6 +41,8 @@ int main(int argc, char *argv[]) {
         if (dir == 'R'){
             for (i = 0; i < numVals; i++){
                 index = i + numRotations;
+				
+				// Until index is less than or equal to the max index, subtract the numbers of values
                 while (index >= numVals){
                     index = index - numVals;
                 }
