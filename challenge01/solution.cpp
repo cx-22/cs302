579517a707a233090038c7da528cbe2026566216
Elias Tadesse Tessema <ckl422@tesla15.eecs.utk.edu>
2025-01-24 16:00:25 -0500

    completed

--- challenge01/solution.cpp
+++ challenge01/solution.cpp
@@ -3,22 +3,49 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-	int numVals = 0, numRotations = 0;
-	char dir = 'X';
+    int numVals = 0, numRotations = 0;
+    char dir = 'X';
+    int i = 0, index = 0;
 
-	int i = 0;
-	while(std::cin >> numVals >> numRotations >> dir){
-		int* nums = new int[numVals];
-		for (int i = 0; i < numVals; i++){
-			std::cin >> nums[i];
-		}
+    while(std::cin >> numVals >> numRotations >> dir){
+        int nums[numVals];
+        int newNums[numVals];
 
-		for (int i = 0; i < numVals; i++){
-			std::cout >> nums[i];
-		}
+        for (i = 0; i < numVals; i++){
+            std::cin >> nums[i];
+        }
 
-	}
+        if (dir == 'L'){
+            for (i = 0; i < numVals; i++){
+                index = i - numRotations;
+                while (index < 0){
+                    index = numVals + index;
+                }
+                newNums[index] = nums[i];
+            }
+        }
 
-	return 0;
+        if (dir == 'R'){
+            for (i = 0; i < numVals; i++){
+                index = i + numRotations;
+                while (index >= numVals){
+                    index = index - numVals;
+                }
+
+                newNums[index] = nums[i];
+            }
+        }
+
+        for (int i = 0; i < numVals; i++){
+            std::cout << newNums[i];
+            if (i < (numVals - 1)){
+                std::cout << " ";
+            }
+        }
+
+        std::cout << std::endl;
+    }
+
+
+    return 0;
 }
-
