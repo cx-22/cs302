7810a1fbec9aa5420de5dd316effe86e89ef7a3e
Elias Tadesse Tessema <ckl422@tesla15.eecs.utk.edu>
2025-03-14 15:01:16 -0400

    completed with comments

--- challenge06/solution.cpp
+++ challenge06/solution.cpp
@@ -1,7 +1,66 @@
-// Challenge 06: Repeated DNA sequences
+/* Challenge 06: Repeated DNA sequences
+ *
+ * Elias Tessema NETID: CKL422
+ *
+ * This program will return all repeating 9 character long strings in
+ * a given DNA sequence
+ *
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
-    return 0;
+
+	unordered_map <string, int> map;
+	vector<string> sortedList;
+	string sequence;
+	string subStr;
+	size_t size = 9;
+
+	while(cin >> sequence){
+	
+		// If its less than (size + 1) chars long then there cant be a repeating
+		// substring of of that size
+		if (sequence.length() < (size + 1)){
+			cout << "-1\n";
+			continue;
+		}
+
+		map.clear();
+		sortedList.clear();
+		
+		// Add every 9 long substring to the hash table, if its already there
+		// add it to the vector
+		for (size_t i = 0; i <= (sequence.length() - size); ++i){
+			subStr = sequence.substr(i, size);
+			unordered_map<string, int>::iterator search = map.find(subStr);
+
+			if(search != map.end()){
+				
+				// Checking if its set to 0 ensures we only push back a string once
+				if (map[subStr] == 0){
+					map[subStr] = map[subStr] + 1;
+					sortedList.push_back(subStr);
+				}
+			} else {
+				map[subStr] = 0;
+			}
+		}
+
+		sort(sortedList.begin(), sortedList.end());
+
+		for (size_t i = 0; i < sortedList.size(); ++i){
+			cout << sortedList[i] << "\n";
+		}
+
+		cout << "-1\n";
+	}
+
+	return 0;
 }
-
-
